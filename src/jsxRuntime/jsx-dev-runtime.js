fd76992f56d9c008d9238cc8be41ba60547402e2
gus4214 <98795239+gus4214@users.noreply.github.com>
2024-12-16 14:29:22 +0900

    ✨ React 제거 후 JsxRuntime: 'automatic' 설정 및 jsx 함수 구현

---
+++ src/jsxRuntime/jsx-dev-runtime.js
@@ -0,0 +1,5 @@
+import { jsx, jsxs, Fragment } from "./jsx-runtime.js";
+export function jsxDEV(type, props, key, isStaticChildren, source, self) {
+  return { type, props, key, __source: source, __self: self };
+}
+export { jsx, jsxs, Fragment };
---
+++ src/main.js
@@ -0,0 +1,4 @@
+import App from "./App.jsx";
+
+const appElement = App();
+console.log("🚀 ~ appElement:", appElement);
