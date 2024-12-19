9bac693e33577293948670388b3299a273c89cb3
gus4214 <98795239+gus4214@users.noreply.github.com>
2024-12-19 19:25:00 +0900

    🔥 패키지 정리

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,9 @@ import { defineConfig } from "vite";
 export default defineConfig({
   esbuild: {
     jsxInject: `import { createElement, Fragment } from '/libs/jsx/createElement.ts'`,
+    // 모든 JSX 파일에 자동으로 import
     jsxFactory: "createElement",
+    // JSX가 createElement 함수로 변환
     jsxFragment: "Fragment",
   },
 });
